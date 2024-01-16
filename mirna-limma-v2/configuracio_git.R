22caa302898c6ac1bdd44164d5be891cb82a3215
bioinfoIdisba <105040797+bioinfoIdisba@users.noreply.github.com>
2024-01-16 11:32:56 +0100

    Add files via upload

---
+++ mirna-limma-v2/configuracio_git.R
@@ -0,0 +1,9 @@
+# configuracio
+library(usethis)
+usethis::edit_git_config()
+library(usethis)
+usethis::use_git()
+usethis::create_github_token()
+library(gitcreds)
+gitcreds::gitcreds_set()
+usethis::use_github()
